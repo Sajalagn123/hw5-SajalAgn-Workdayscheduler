d1848d60338b19872cebb41a7b97c17c388d6a37
Sajal Agnihotri <sajalagn3@gmail.com>
2022-04-21 00:19:01 -0700

    initial commit

---
+++ planner.js
@@ -0,0 +1,6 @@
+// Step 1: Have current day displayed at the top of the calender
+// Step 2: Have timeblocks for standard business hours presented
+// Step 3: Have time blocks color coded to indicate whether they are in the past, present, or future
+// Step 4: Make user able to enter an event when they click on a timeblock
+// Step 5: Have text be saved in local storage when user clicks on an event
+// Step 6: Have content user entered remain when the page is refreshed
