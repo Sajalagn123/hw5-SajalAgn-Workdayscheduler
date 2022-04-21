2fdf5f8b2db9a7e7560410356732acf49ebd1666
Sajal Agnihotri <sajalagn3@gmail.com>
2022-04-21 15:48:24 -0700

    update3

--- Develop/planner.js
+++ Develop/planner.js
@@ -1,9 +1,13 @@
-// Step 1: Have current day displayed at the top of the calender
-// Step 2: Have timeblocks for standard business hours presented
-// Step 3: Have time blocks color coded to indicate whether they are in the past, present, or future
-// Step 4: Make user able to enter an event when they click on a timeblock
+// Step : Have current day displayed at the top of the calender
+// Step : Have timeblocks for standard business hours presented
+// Step : Have time blocks color coded to indicate whether they are in the past, present, or future
+// Step : Make user able to enter an event when they click on a timeblock
 // Step 5: Have text be saved in local storage when user clicks on an event
 // Step 6: Have content user entered remain when the page is refreshed
+
+var todayDate = moment().format("dddd, MMMM Do, YYYY");
+$('#currentDay').append(todayDate);
+
 
 function makeTimeblocks(hour, existingTodo = ""){
   var currentHour = new Date().getHours();
@@ -21,6 +25,5 @@ function makeTimeblocks(hour, existingTodo = ""){
 for(var i = 9; i < 18; i++){
   makeTimeblocks(i);
 }
-
 
 
