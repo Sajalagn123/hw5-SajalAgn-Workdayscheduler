95f36dcd123dac8cb6c5145d5002b17e4149a40b
Sajal Agnihotri <sajalagn3@gmail.com>
2022-04-22 00:14:14 -0700

    update5

--- Develop/planner.js
+++ Develop/planner.js
@@ -5,25 +5,45 @@
 // Step 5: Have text be saved in local storage when user clicks on an event
 // Step 6: Have content user entered remain when the page is refreshed
 
+// Displays today's date at the top of the page
 var todayDate = moment().format("dddd, MMMM Do, YYYY");
 $('#currentDay').append(todayDate);
 
-
+var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+// Creates the timeblocks for each hour
 function makeTimeblocks(hour, existingTodo = ""){
   var currentHour = new Date().getHours();
+  // Color codes time blocks
   var threeTime = "future";
-  if(currentHour > hour) threeTime = "past";
-  if(currentHour === hour) threeTime = "present"
+  if(currentHour > hour + 9) threeTime = "past";
+  if(currentHour === hour + 9) threeTime = "present"
   $(".container").append($(
     `<div class="row time-block">
-        <div class="hour col-1">${hour}:00</div>
-        <textarea name="" id="" cols="30" rows="3" class="description col-9 ${threeTime}">${existingTodo}</textarea>
-        <div class="btn saveBtn col-2">Save</div>
+        <div class="hour col-1">${hours[hour]}</div>
+        <textarea name="" id="${hours[hour]}" cols="30" rows="3" class="description col-9 ${threeTime}">${existingTodo}</textarea>
+        <button class="btn saveBtn col-2">Save</button>
   </div>`));
 }
 
-for(var i = 9; i < 18; i++){
+// Repeats time blocks until 5pm
+for(var i = 0; i < 9; i++){
   makeTimeblocks(i);
 }
 
+// selects all elements with a the class saveBtn
+var btns = document.querySelectorAll(".saveBtn");
 
+// For loop that adds an event listener to each button
+for (var i = 0; i < btns.length; i++){
+btns[i].addEventListener("click", functionSaveBtn);
+}
+
+
+function functionSaveBtn(event){
+var taskValue = event.target.parentNode.children[1].value;
+var taskKey = event.target.parentNode.children[1].id;
+
+console.log("key values ", taskKey, taskValue);
+
+localStorage.setItem(taskKey, taskValue);
+}
